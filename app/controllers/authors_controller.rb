5f8523b14a23330214e031e8b2deaa7e55ca0c26
libaumann <lisa.baumann@student.hpi.de>
2022-11-04 20:50:56 +0100

    Test passed for #5

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,13 @@
+class AuthorsController < ApplicationController
+  def index
+  end
+  def new 
+    @author = Author.new
+  end 
+  def create
+    @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
+ 
+    @author.save
+    redirect_to root_path, notice: 'Success!'
+  end
+end
