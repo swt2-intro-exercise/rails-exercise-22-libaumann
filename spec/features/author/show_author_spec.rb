64f744b624424291b14123df8061fc159b9b0073
libaumann <lisa.baumann@student.hpi.de>
2022-11-05 11:46:52 +0100

    Passing test for #6 again

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -6,5 +6,6 @@ describe "Show author page", type: :feature do
     @alan = FactoryBot.create :author
     visit author_path(@alan)
     expect(page).to have_text("Alan Turing")
+    expect(page).to have_text("http://wikipedia.de/Alan_Turing")
   end
 end
