e0e96f1054ce7910df69e15ff10024ad6f7dbd2b
libaumann <lisa.baumann@student.hpi.de>
2022-11-07 15:10:46 +0100

    Passing test for #18

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,9 @@
 class Author < ApplicationRecord
   validates :last_name, presence: true,
                     length: { minimum: 1 }
+  has_and_belongs_to_many :papers
+  has_many :authors_papers
+  has_many :papers, :through => :authors_papers
   def name() 
 	first_name + " " + last_name
   end
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,4 +2,7 @@ class Paper < ApplicationRecord
   validates :title, presence: true
   validates :venue, presence: true
   validates :year, presence: true, numericality: { only_integer: true }
+  has_and_belongs_to_many :authors
+  has_many :authors_papers
+  has_many :authors, :through => :authors_papers
 end
